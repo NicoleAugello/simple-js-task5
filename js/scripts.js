ceb4c7efea4b133433a284e12607c00a2a367621
NicoleAugello <nicole.augello@ontariotechu.net>
2025-09-23 18:02:54 -0400

    Task 5- Part 2: IIFE
    wrap your pokemonList array in an IIFE to avoid accidentally accessing the global state.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
+//  TASK  5
+
+// Declaration of pokemon array - wrapped in an IIFE
+(function () {
 let pokemonList = [
     { name: 'Jigglypuff', height: 5, types:  ['poision', 'bug']},
     { name: 'Charmeleon', height: 11, types:  ['ground', 'steel']},
     { name: 'Raichu', height: 8, types:  ['fire', 'flying']}
-];
+  ];
+})();
 
-//  TASK  5 
 // Declaration of pokemon array - forEach() function
 
 pokemonList.forEach(function(pokemon) {
